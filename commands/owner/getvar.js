c74dda3e464e4703824b12289e7a4aef1f0b7a47
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-05-17 17:59:09 +0300

    Create getvar.js

---
+++ commands/owner/getvar.js
@@ -0,0 +1,23 @@
+import Heroku from "heroku-client";
+
+const Getvar = async(client, m, text, Owner, NotOwner, herokuapi, appname) => {
+
+if (!Owner) return m.reply(NotOwner);
+
+if (!text) return m.reply("provide a value. Example:\ngetvar PREFIX")
+try {
+
+const heroku = new Heroku({
+              token: herokuapi,
+            });
+            let baseURI = "/apps/" + appname;
+        let mok = await heroku.get(baseURI+'/config-vars')
+        for (let vr in mok) {
+        if( text ===vr ) return m.reply( vr+'= '+mok[vr]);
+        } 
+
+        } catch(e) {m.reply('Error below has occured' + e)}
+
+}
+
+export default Getvar;
