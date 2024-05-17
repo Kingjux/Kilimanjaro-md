d98cf2642bfb0632353b3e165770dfe9ce692859
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-05-17 20:49:22 +0300

    Create toimg.js

---
+++ commands/edit/toimg.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+const imgSticker = async (client, m, quoted, mime, exec, getRandom) => {
+if (!quoted) return m.reply('Tag a static video with the command!'); 
+    if (!/webp/.test(mime)) return m.reply(`Tag a sticker with the command`); 
+  
+    let media = await client.downloadAndSaveMediaMessage(quoted); 
+    let mokaya = await getRandom('.png'); 
+    exec(`ffmpeg -i ${media} ${mokaya}`, (err) => { 
+   fs.unlinkSync(media); 
+   if (err) m.reply(err)
+   let buffer = fs.readFileSync(mokaya); 
+   client.sendMessage(m.chat, { image: buffer, caption: `Converted by Kilimanjaro! 💚`}, { quoted: m}) 
+   fs.unlinkSync(kingjux); 
+    }); 
+    } 
+
+export default imgSticker;
